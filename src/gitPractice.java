de8e4988f7d17c8f3bdf20dbd40f02071b53d4f6
Pushpinder Singh <117393617+pushpinder024@users.noreply.github.com>
2022-11-11 03:14:27 -0500

    Update gitPractice.java

--- src/gitPractice.java
+++ src/gitPractice.java
@@ -4,6 +4,8 @@ public class gitPractice {
     public static void main(String[] args) {
        
       System.out.println("This is me, practicing github");  
+      String ss  = "Pushpinder";
+      char mk = 'f';
 
     }
     public static void master(int a){
