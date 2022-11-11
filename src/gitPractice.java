51bef26c28e3b934694073d580e2ad8774bf2969
pushpinder024 <pushpindersingh024@gmail.com>
2022-11-11 03:16:15 -0500

    new final changes

--- src/gitPractice.java
+++ src/gitPractice.java
@@ -1,9 +1,11 @@
-
+import java.util.Scanner;
 
 public class gitPractice {
     public static void main(String[] args) {
        
-      System.out.println("This is me, practicing github");  
+      System.out.println("This is me, practicing github");
+      Scanner sc  = new Scanner(System.in);
+      int x =  sc.nextInt();
 
     }
     public static void master(int a){
