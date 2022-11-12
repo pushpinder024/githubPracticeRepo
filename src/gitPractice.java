9cee20bff1da2f8eb9dd674f88488017dd743037
pushpinder024 <pushpindersingh024@gmail.com>
2022-11-12 10:12:38 -0500

    changes made in gitPractice File

--- src/gitPractice.java
+++ src/gitPractice.java
@@ -2,28 +2,31 @@ import java.util.Scanner;
 
 public class gitPractice {
     public static void main(String[] args) {
-       
-
-      System.out.println("This is me, practicing github");
-      Scanner sc  = new Scanner(System.in);
-      int x =  sc.nextInt();
 
 
-      System.out.println("This is me, practicing github");
-main();
+        System.out.println("This is me, practicing github");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
 
-      System.out.println("This is me, practicing github");  
+
+        System.out.println("This is me, practicing github");
+
+        System.out.println("This is me, practicing github");
         String ssss = null;
 
-      String ss  = null;
+        String ss = null;
 
-      String gggd = null;
+        String gggd = null;
 
-      main();
 
-      char mk = 'f';
+        char mk = 'f';
 
     }
-    public static void master(int a){
 }
 
+//    I PULL A REPO INTO MY PUSHPINDER BRANCH
+//  I RESOLVED THE MERGE CONFLICTS
+// THEN I TRY TO PUSH THE FILES TO THE REMOTE REPO
+//IT FAILS
+// THEN I MERGE PUSHPINDER BRANCH TO NEWBRANCH AT LOCAL
+// THEN I PUSH THE CHANGES, PUSHED SUCCESSFULLY , BUT COMMIT ID IS ONE I MADE AT PUSHPINDER BRANCH
